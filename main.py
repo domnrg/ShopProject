1442a5c7c9560c29574bbe69eae2a0f09a506618
Alexander Yakovenko <yakovenko.berdyansk@gmail.com>
2025-08-24 23:40:00 +0300

    Add routing in main.py

--- main.py
+++ main.py
@@ -8,21 +8,28 @@ serverPort = 8080
 class MyServer(BaseHTTPRequestHandler):
     def do_GET(self):
         """ Метод для обработки GET-запросов """
-        try:
-            with open("Contacts.html", "r", encoding="utf-8") as f:
-                html_content = f.read()
-        except FileNotFoundError:
+        routes = {
+            "/": "main.html",
+            "/catalog": "Catalog.html",
+            "/category": "Category.html",
+            "/contacts": "Contacts.html",
+        }
+        file_name = routes.get(self.path)
+
+        if file_name and os.path.exists(file_name):
+            # Отправляем страницу
+            self.send_response(200)
+            self.send_header("Content-type", "text/html; charset=utf-8")
+            self.end_headers()
+            with open(file_name, "r", encoding="utf-8") as f:
+                self.wfile.write(bytes(f.read(), "utf-8"))
+
+        else:
             self.send_response(404)
             self.send_header("Content-type", "text/html; charset=utf-8")
             self.end_headers()
             self.wfile.write(bytes("<h1>404 Страница не найдена</h1>", "utf-8"))
             return
-
-        # Отправляем страницу
-        self.send_response(200)
-        self.send_header("Content-type", "text/html; charset=utf-8")
-        self.end_headers()
-        self.wfile.write(bytes(html_content, "utf-8"))
 
 if __name__ == "__main__":
     webServer = HTTPServer((hostName, serverPort), MyServer)
